fca82fdbbd1584a870c8cb98fbe192fa8ea8aeea
Enzo <enzo.dobele@yahoo.fr>
2025-08-05 17:37:34 +0200

    input in string done

--- minishell.h
+++ minishell.h
@@ -6,7 +6,29 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+typedef	enum	e_token_type
+{
+	WORD,
+	PIPE,
+	REDIR_IN, // <
+	REDIR_OUT, // >
+	REDIR_APPEND, // >>
+	HEREDOC, // <<
+	VAR, // $
+	SEPARATOR // ;
+}	t_token_type;
+
+typedef	struct	s_token
+{
+	char			*string;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
 
 void	handle_sigint(int signum);
+void	input_in_token(char *input);
+char	*input_in_string(int i, char *input);
 
 #endif
--- tokenizer.c
+++ tokenizer.c
@@ -1,2 +1,43 @@
 #include "minishell.h"
 
+void input_in_token(char *input)
+{
+	int		i;
+	t_token	token;
+	char	*string;
+
+	i = 0;
+	while (input[i])
+	{
+		while (input[i] == ' ')
+			i++;
+		if (input[i])
+		{
+			string = input_in_string(i, input);
+		}
+		while (input[i] && input[i] == ' ')
+			i++;
+	}
+}
+char *input_in_string(int i, char *input)
+{
+	int		j;
+	int		nbr_letters;
+	char	*string;
+
+	nbr_letters = 0;
+	while (input[i] != ' ' && input[i])
+	{
+		nbr_letters++;
+		i++;
+	}
+	string = malloc(sizeof (char) * (nbr_letters + 1));
+	if (!string)
+		return (NULL);
+	i -= nbr_letters;
+	j = 0;
+	while (j < nbr_letters)
+		string[j++] = input[i++];
+	string[j] = '\0';
+	return (string);
+}
