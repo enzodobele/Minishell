8295f40f9256d885aa1bdcfaa09112e65566620f
mzimeris <mzimeris@student.42.fr>
2025-08-08 15:34:05 +0200

    cd handler a securiser

--- src/m_exec.c
+++ src/m_exec.c
@@ -6,7 +6,7 @@
 /*   By: mzimeris <mzimeris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 13:13:37 by mzimeris          #+#    #+#             */
-/*   Updated: 2025/08/08 15:11:30 by mzimeris         ###   ########.fr       */
+/*   Updated: 2025/08/08 15:24:36 by mzimeris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@ int	handle_cd(t_token *token)
 		return (chdir(getenv("HOME")));
 	else if (token->string[0] == '/' || token->string[0] == '~')
 		return (chdir(token->string));
+	else if (ft_strcmp(token->string, "-") == 0)
+		return (chdir(getenv("OLDPWD")));
+	else if (ft_strcmp(token->string, ".") == 0)
+		return (chdir(getenv("PWD")));
+	else if (ft_strcmp(token->string, "..") == 0)
+		return (chdir("..")); // TODO: handle relative paths properly
 	return (-1);
 }
 
@@ -58,28 +64,28 @@ int	handle_pwd(void)
 	
 // }
 
-// int	exec_builtins(t_token *token)
-// {
-// 	if (!token || !token->string)
-// 		return (0);
-// 	if (ft_strcmp(token->string, "echo") == 0) // besoin du param
-// 		return (handle_echo(token));
-// 	if (ft_strcmp(token->string, "cd") == 0) // besoin du chemin
-// 		return (handle_cd(token));
-// 	if (ft_strcmp(token->string, "pwd") == 0)
-// 		return (handle_pwd(token));
-// 	if (ft_strcmp(token->string, "export") == 0)
-// 		return (handle_export(token));
-// 	if (ft_strcmp(token->string, "unset") == 0)
-// 		return (handle_unset(token));
-// 	if (ft_strcmp(token->string, "env") == 0)
-// 		return (handle_env(token));
-// 	if (ft_strcmp(token->string, "exit") == 0)
-// 		return (handle_exit(token));
-// 	else
-// 		return (find_and_exec(token));
-// 	return (0);
-// }
+int	exec_builtins(t_token *token)
+{
+	if (!token || !token->string)
+		return (0);
+	// if (ft_strcmp(token->string, "echo") == 0) // besoin du param
+	// 	return (handle_echo(token));
+	if (ft_strcmp(token->string, "cd") == 0)
+		return (handle_cd(token->next));
+	if (ft_strcmp(token->string, "pwd") == 0)
+		return (handle_pwd());
+	// if (ft_strcmp(token->string, "export") == 0)
+	// 	return (handle_export(token->next));
+	// if (ft_strcmp(token->string, "unset") == 0)
+	// 	return (handle_unset(token));
+	// if (ft_strcmp(token->string, "env") == 0)
+	// 	return (handle_env(token));
+	// if (ft_strcmp(token->string, "exit") == 0)
+	// 	return (handle_exit(token));
+	// else
+	// 	return (find_and_exec(token));
+	return (0);
+}
 
 // int	exec(t_token *token)
 // {
