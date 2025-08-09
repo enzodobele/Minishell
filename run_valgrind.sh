4bad7d237db13213449f16c30388471e89e7a6ee
Hermes <mathieuhermesfr@gmail.com>
2025-08-10 00:39:41 +0200

    script valgrind

---
+++ run_valgrind.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Compilation..."
+make
+
+echo "Lancement de valgrind avec suppression des erreurs readline..."
+
+valgrind --suppressions=.valgrind_suppress \
+         --leak-check=full \
+         --show-leak-kinds=all \
+         --track-origins=yes \
+         ./minishell
