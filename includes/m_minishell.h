c730d071833437bf290cb7b7fc2410bdace5468d
Hermes <mathieuhermesfr@gmail.com>
2025-08-20 21:32:38 +0200

    merge du nouveau main

--- includes/m_minishell.h
+++ includes/m_minishell.h
@@ -6,7 +6,7 @@
 /*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 13:12:43 by mzimeris          #+#    #+#             */
-/*   Updated: 2025/08/20 21:16:50 by zoum             ###   ########.fr       */
+/*   Updated: 2025/08/20 21:32:05 by zoum             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n);
 void		ft_putstr_fd(char *s, int fd);
 
 // Cleanup functions
-void		cleanup_minishell(char **input, t_token **token, t_command **cmd, t_env **env);
-void		cleanup_and_exit(char **input, t_token **token, t_command **cmd, t_env **env);
+void		cleanup_minishell(char **input, t_token **token, t_command **cmd, t_env *env);
+void		cleanup_and_exit(char **input, t_token **token, t_command **cmd, t_env *env);
 
 #endif /* M_MINISHELL_H */
