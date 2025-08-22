b438958a8479e93c8f1963f1725e0d93fd790b7a
Hermes <mathieuhermesfr@gmail.com>
2025-08-22 13:45:52 +0200

    cette fois...

--- src/m_exec.c
+++ src/m_exec.c
@@ -6,7 +6,7 @@
 /*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 13:13:37 by mzimeris          #+#    #+#             */
-/*   Updated: 2025/08/22 13:25:01 by zoum             ###   ########.fr       */
+/*   Updated: 2025/08/22 13:44:04 by zoum             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,8 @@
 
 int	exec_builtins(t_command *command, t_env *env)
 {
-	 //  printf("[exec_builtins] Entrée dans la fonction.\n");
 	if (!command || !command->cmd)
-	{
-		 //  printf("[exec_builtins] Commande ou cmd est NULL, retourne 0.\n");
 		return (0);
-	}
-	 //  printf("[exec_builtins] Commande à exécuter: %s\n", command->cmd->string);
 	if (ft_strcmp(command->cmd->string, "cd") == 0)
 		return (handle_cd(env, command), 1);
 	if (ft_strcmp(command->cmd->string, "pwd") == 0)
@@ -37,7 +32,6 @@ int	exec_builtins(t_command *command, t_env *env)
 		return (handle_env(env), 1);
 	if (ft_strcmp(command->cmd->string, "exit") == 0)
 		return (handle_exit(command, env));
-	 //  printf("[exec_builtins] Commande n'est pas un builtin, retourne 0.\n");
 	return (0);
 }
 
@@ -70,103 +64,70 @@ char	**build_argv(t_command *command)
 
 int	exec_command(t_command *command, t_env *env)
 {
-	int builtin_result;
-	char **argv;
+	int		builtin_result;
+	char	**argv;
 
-	 //  printf("[exec_command] Entrée dans la fonction pour la commande: %s\n", command->cmd->string);
 	if (!command || !env)
-	{
-		 //  printf("[exec_command] Commande ou env est NULL, retourne -1.\n");
 		return (-1);
-	}
 	builtin_result = exec_builtins(command, env);
-	 //  printf("[exec_command] Résultat de exec_builtins: %d\n", builtin_result);
 	if (builtin_result == 42)
-	{
-		 //  printf("[exec_command] C'est un builtin 'exit', retourne 42.\n");
 		return (42);
-	}
 	else if (builtin_result)
-	{
-		 //  printf("[exec_command] C'est un builtin, retourne 0 (pas d'erreur).\n");
 		return (0);
-	}
 	else
 	{
 		if (check_command(env, command) < 0)
-		{
-			 //  printf("[exec_command] Commande introuvable ou problème de permissions, retourne -1.\n");
 			return (handle_command_error(command, -1), -1);
-		}
 		argv = build_argv(command);
 		if (!argv)
 			exit(127);
-		 //  printf("[exec_command] Appel de execve avec la commande: %s\n", command->cmd->string);
 		execve(command->cmd->string, argv, env->envp);
 		free(argv);
 		perror("execve");
 	}
-	 //  printf("[exec_command] Fin de la fonction.\n");
 	return (0);
 }
 
 static void	_clean_child_fds(t_pipe_data *pipe_data)
 {
-	 //  printf("[_clean_child_fds] Nettoyage des FDs pour l'enfant.\n");
-	 //  printf("[_clean_child_fds] pipe_data->in_fd: %d, pipe_data->out_fd: %d, pipe_data->is_piped: %d\n", pipe_data->in_fd, pipe_data->out_fd, pipe_data->is_piped);
 	if (pipe_data->in_fd > 0)
 	{
-		 //  printf("[_clean_child_fds] Redirection de STDIN (fd 0) vers in_fd (%d).\n", pipe_data->in_fd);
 		if (dup2(pipe_data->in_fd, STDIN_FILENO) == -1)
 			perror("dup2 STDIN");
 		if (close(pipe_data->in_fd) == -1)
 			perror("close in_fd");
-		 //  printf("[_clean_child_fds] in_fd (%d) fermé.\n", pipe_data->in_fd);
 	}
 	if (pipe_data->is_piped)
 	{
-		 //  printf("[_clean_child_fds] C'est un pipe. Fermeture de pipe_fd[0] (%d) et redirection de STDOUT (fd 1) vers pipe_fd[1] (%d).\n", pipe_data->pipe_fd[0], pipe_data->pipe_fd[1]);
 		if (close(pipe_data->pipe_fd[0]) == -1)
 			perror("close pipe_fd[0]");
 		if (dup2(pipe_data->pipe_fd[1], STDOUT_FILENO) == -1)
 			perror("dup2 STDOUT");
 		if (close(pipe_data->pipe_fd[1]) == -1)
 			perror("close pipe_fd[1]");
-		 //  printf("[_clean_child_fds] pipe_fd[1] (%d) fermé.\n", pipe_data->pipe_fd[1]);
 	}
 	else if (pipe_data->out_fd > 1)
 	{
-		 //  printf("[_clean_child_fds] Redirection de STDOUT (fd 1) vers out_fd (%d).\n", pipe_data->out_fd);
 		if (dup2(pipe_data->out_fd, STDOUT_FILENO) == -1)
 			perror("dup2 STDOUT");
 		if (close(pipe_data->out_fd) == -1)
 			perror("close out_fd");
-		 //  printf("[_clean_child_fds] out_fd (%d) fermé.\n", pipe_data->out_fd);
 	}
-	 //  printf("[_clean_child_fds] Nettoyage des FDs terminé.\n");
 }
 
 int	exec_child(t_env *env, t_command *command, t_pipe_data *pipe_data)
 {
 	int	check_result;
 
-	 //  printf("[exec_child] Processus enfant lancé pour la commande: %s\n", command->cmd->string);
 	if (!command || !command->cmd || !command->cmd->string)
-	{
-		 //  printf("[exec_child] Commande ou cmd est NULL, exit(127).\n");
 		exit(127);
-	}
 	_clean_child_fds(pipe_data);
-	 //  printf("[exec_child] FDs nettoyés et redirigés.\n");
 	check_result = check_command(env, command);
 	if (check_result < 0)
 	{
-		 //  printf("[exec_child] Commande introuvable ou permissions refusées, exit(127).\n");
 		handle_command_error(command, check_result);
 		exit(127);
 	}
-	 //  printf("[exec_child] Commande valide. Appel de exec_command.\n");
 	exec_command(command, env);
-	 //  printf("[exec_child] Échec d'exécution de la commande (exec_command a échoué), exit(127).\n");
 	exit(127);
 }
--- src/m_pipeline.c
+++ src/m_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 13:13:37 by mzimeris          #+#    #+#             */
-/*   Updated: 2025/08/22 13:25:01 by zoum             ###   ########.fr       */
+/*   Updated: 2025/08/22 13:38:42 by zoum             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,26 @@ static int	_handle_out_redir(t_redirect *redir)
 {
 	int	fd;
 
-	 //  printf("[_handle_out_redir] Tente d'ouvrir le fichier de sortie: %s\n", redir->filename);
 	fd = -1;
 	if (redir->type == REDIR_OUT)
 	{
 		fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		 //  printf("[_handle_out_redir] Type: REDIR_OUT (>). FD obtenu: %d\n", fd);
+
 	}
 	else if (redir->type == REDIR_APPEND)
 	{
 		fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		 //  printf("[_handle_out_redir] Type: REDIR_APPEND (>>). FD obtenu: %d\n", fd);
+
 	}
 	return (fd);
 }
 
 static int	_get_last_output_fd(t_command *cmd, t_pipe_data *pipe_data)
 {
-	t_redirect *redir;
-	int fd;
-	int prev_fd;
+	t_redirect	*redir;
+	int			fd;
+	int			prev_fd;
 
-	 //  printf("[_get_last_output_fd] Recherche de la dernière redirection de sortie.\n");
 	fd = -1;
 	prev_fd = -1;
 	pipe_data->outfile_error = 0;
@@ -48,13 +46,13 @@ static int	_get_last_output_fd(t_command *cmd, t_pipe_data *pipe_data)
 		{
 			if (prev_fd != -1)
 			{
-				 //  printf("[_get_last_output_fd] Fermeture du FD de la redirection de sortie précédente: %d\n", prev_fd);
+
 				close(prev_fd);
 			}
 			fd = _handle_out_redir(redir);
 			if (fd < 0)
 			{
-				 //  printf("[_get_last_output_fd] Erreur lors de la redirection de sortie. Fichier: %s\n", redir->filename);
+
 				perror(redir->filename);
 				pipe_data->outfile_error = 1;
 				return (fd);
@@ -64,20 +62,15 @@ static int	_get_last_output_fd(t_command *cmd, t_pipe_data *pipe_data)
 		redir = redir->next;
 	}
 	if (prev_fd != -1)
-	{
-		 //  printf("[_get_last_output_fd] Le dernier FD de sortie est %d.\n", prev_fd);
 		return (prev_fd);
-	}
-	 //  printf("[_get_last_output_fd] Aucune redirection de sortie trouvée.\n");
 	return (-1);
 }
 
-static int _get_last_input_fd(t_command *cmd)
+static int	_get_last_input_fd(t_command *cmd)
 {
-	t_redirect *redir;
-	int fd;
+	t_redirect	*redir;
+	int			fd;
 
-	 //  printf("[_get_last_input_fd] Recherche de la dernière redirection d'entrée.\n");
 	fd = -1;
 	redir = cmd->redirects;
 	while (redir)
@@ -86,14 +79,14 @@ static int _get_last_input_fd(t_command *cmd)
 		{
 			if (fd > 0)
 			{
-				 //  printf("[_get_last_input_fd] Fermeture du FD de la redirection d'entrée précédente: %d\n", fd);
+
 				close(fd);
 			}
 			fd = open(redir->filename, O_RDONLY);
-				 //  printf("[_get_last_input_fd] Type: REDIR_IN (<). FD obtenu: %d\n", fd);
+
 			if (fd < 0)
 			{
-				 //  printf("[_get_last_input_fd] Erreur lors de la redirection d'entrée. Fichier: %s\n", redir->filename);
+
 				perror(redir->filename);
 				return (-1);
 			}
@@ -105,93 +98,74 @@ static int _get_last_input_fd(t_command *cmd)
 
 static void	_pipexecution_clean_fds(t_pipe_data *pipe_data)
 {
-	 //  printf("[_pipexecution_clean_fds] Nettoyage des FDs pour la prochaine exécution.\n");
-	 //  printf("[_pipexecution_clean_fds] État initial: redir_in_fd=%d, in_fd=%d, outfile_error=%d\n", pipe_data->redir_in_fd, pipe_data->in_fd, pipe_data->outfile_error);
+
 	if (pipe_data->outfile_error)
 	{
-		 //  printf("[_pipexecution_clean_fds] Erreur de fichier de sortie détectée. Fermeture des FDs d'entrée.\n");
+
 		if (pipe_data->redir_in_fd > 0)
 		{
-			 //  printf("[_pipexecution_clean_fds] Fermeture de redir_in_fd (%d).\n", pipe_data->redir_in_fd);
+
 			close(pipe_data->redir_in_fd);
 		}
 		if (pipe_data->in_fd > 0)
 		{
-			 //  printf("[_pipexecution_clean_fds] Fermeture de in_fd (%d).\n", pipe_data->in_fd);
+
 			close(pipe_data->in_fd);
 		}
 		pipe_data->in_fd = -1;
-		 //  printf("[_pipexecution_clean_fds] in_fd mis à -1.\n");
-		return;
+		return ;
 	}
 	if (pipe_data->redir_in_fd != -1)
 	{
-		 //  printf("[_pipexecution_clean_fds] Redirection d'entrée trouvée. Mise à jour de in_fd.\n");
+
 		if (pipe_data->in_fd > 0)
 		{
-			 //  printf("[_pipexecution_clean_fds] Fermeture de l'ancien in_fd du pipe: %d.\n", pipe_data->in_fd);
+
 			close(pipe_data->in_fd);
 		}
 		pipe_data->in_fd = pipe_data->redir_in_fd;
-		 //  printf("[_pipexecution_clean_fds] in_fd mis à %d (redir_in_fd).\n", pipe_data->in_fd);
+
 	}
 	else if (pipe_data->in_fd == -1)
-	{
-		 //  printf("[_pipexecution_clean_fds] Aucune redirection d'entrée. in_fd reste l'entrée du pipe précédent ou STDIN.\n");
-			pipe_data->in_fd = STDIN_FILENO;
-	}
-	 //  printf("[_pipexecution_clean_fds] État final: redir_in_fd=%d, in_fd=%d\n", pipe_data->redir_in_fd, pipe_data->in_fd);
+		pipe_data->in_fd = STDIN_FILENO;
 }
 
-int pipexecution(t_env *env, t_command *cmd)
+int	pipexecution(t_env *env, t_command *cmd)
 {
-	t_pipe_data *pipe_data;
+	t_pipe_data	*pipe_data;
 
-	 //  printf("[pipexecution] Entrée dans la fonction. Démarrage de l'exécution des commandes en chaîne.\n");
 	pipe_data = malloc(sizeof(t_pipe_data));
 	if (!pipe_data)
-	{
-		 //  printf("[pipexecution] Échec de l'allocation mémoire pour pipe_data.\n");
 		return (-1);
-	}
 	*pipe_data = (t_pipe_data){0};
 	while (cmd)
 	{
 		pipe_data->redir_in_fd = -1;
-        pipe_data->out_fd = -1;
-		 //  printf("[pipexecution] Traitement de la commande: %s\n", cmd->cmd->string);
-		if (cmd && cmd->redirects && cmd->redirects->type && cmd->redirects->type == REDIR_IN)
+		pipe_data->out_fd = -1;
+		if (cmd && cmd->redirects && cmd->redirects->type
+			&& cmd->redirects->type == REDIR_IN)
 		{
-			 //  printf("[pipexecution] Des redirections sont présentes.\n");
 			pipe_data->redir_in_fd = _get_last_input_fd(cmd);
 			if (pipe_data->redir_in_fd < 0)
-			{
-				 //  printf("[pipexecution] Échec de la redirection d'entrée. Sortie.\n");
 				return (free(pipe_data), -1);
-			}
 		}
-		if (cmd && cmd->redirects && cmd->redirects->type && (cmd->redirects->type == REDIR_OUT || cmd->redirects->type == REDIR_APPEND))
+		if (cmd && cmd->redirects && cmd->redirects->type
+			&& (cmd->redirects->type == REDIR_OUT
+				|| cmd->redirects->type == REDIR_APPEND))
 		{
 			pipe_data->out_fd = _get_last_output_fd(cmd, pipe_data);
 			if (pipe_data->outfile_error)
-			{
-				 //  printf("[pipexecution] Échec de la redirection de sortie. Sortie.\n");
 				return (free(pipe_data), -1);
-			}
 		}
 		_pipexecution_clean_fds(pipe_data);
-		 //  printf("[pipexecution] Appel de fork_and_exec pour la commande.\n");
 		pipe_data->in_fd = fork_and_exec(env, cmd, pipe_data);
 		if (pipe_data->in_fd < 0 || pipe_data->outfile_error)
 		{
-			 //  printf("[pipexecution] Erreur dans fork_and_exec ou outfile_error est vrai. Sortie.\n");
 			free(pipe_data);
 			return (wait_for_children(), -1);
 		}
 		cmd = cmd->next;
-			 //  printf("[pipexecution] Passe à la commande suivante dans le pipe.\n");
 	}
-	 //  printf("[pipexecution] Toutes les commandes ont été lancées. Attente des processus enfants.\n");
 	free(pipe_data);
 	return (wait_for_children());
 }
