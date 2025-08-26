8d549b080e57851b19d820e201d7e737b3d46610
mzimeris <mzimeris@student.42.fr>
2025-08-26 19:07:27 +0200

    cat double prompt ok

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@ void	handle_sigint(int signum)
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
 	rl_on_new_line();
-	rl_redisplay();
+	// rl_redisplay();
 }
 
 char	*handle_multiline_input(char *input)
